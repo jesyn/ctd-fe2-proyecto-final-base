ee5925ca0de425acf3cb77d685eb95510380f721
Jesica Muñoz <jesydynathos@gmail.com>
2023-06-29 19:53:58 -0300

    fix cita test

--- src/features/quote/Cita.spec.tsx
+++ src/features/quote/Cita.spec.tsx
@@ -13,11 +13,30 @@ const data = [
     image: "imagen",
     characterDirection: "right",
   },
+  {
+    quote:
+      "I believe the children are the future... Unless we stop them nowThese are my only friends...grown-up nerds like Gore Vidal. And even he's kissed more boys than I ever will.",
+    character: "Lisa",
+    image: "imagen",
+    characterDirection: "right",
+  },
 ];
 
-export const handlers = [
-  rest.get(API_URL, (req, res, ctx) => {
-    return res(ctx.json(data), ctx.status(200), ctx.delay(150));
+const validQuery = data.find((q) => q.character);
+
+const handlers = [
+  rest.get(`${API_URL}`, (req, res, ctx) => {
+    const character = req.url.searchParams.get("character");
+
+    if (character === null) {
+      return res(ctx.json([data[1]]), ctx.delay(150));
+    }
+
+    if (validQuery) {
+      return res(ctx.json([validQuery]));
+    }
+
+    return res(ctx.json([]), ctx.delay(150));
   }),
 ];
 
@@ -58,9 +77,10 @@ describe("Cita", () => {
 
       const input = screen.getByRole("textbox", { name: "Author Cita" });
       const buttonSearch = await screen.findByLabelText(/Obtener Cita/i);
-      userEvent.click(input);
-      userEvent.keyboard("homer");
-      userEvent.click(buttonSearch);
+      await userEvent.click(input);
+      await userEvent.keyboard("homer");
+      await userEvent.click(buttonSearch);
+
       await waitFor(() => {
         expect(
           screen.getByText(
@@ -77,14 +97,69 @@ describe("Cita", () => {
 
       const input = screen.getByRole("textbox", { name: "Author Cita" });
       const buttonSearch = await screen.findByLabelText(/Obtener Cita/i);
-      userEvent.click(input);
-      userEvent.clear(input);
-      userEvent.keyboard("homeroo");
+      await userEvent.click(input);
+      await userEvent.clear(input);
+      await userEvent.keyboard("homdfda");
       userEvent.click(buttonSearch);
 
       await waitFor(() => {
         expect(
           screen.getByText(/No se encontro ninguna cita/i)
+        ).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe("Cuando oprimo el botón borrar", () => {
+    it("Deberia borrar la cita", async () => {
+      render(<Cita />);
+
+      const input = screen.getByRole("textbox", { name: "Author Cita" });
+
+      await userEvent.click(input);
+      await userEvent.clear(input);
+      await userEvent.keyboard("homer");
+
+      await waitFor(() => {
+        expect(screen.getByDisplayValue("homer")).toBeInTheDocument();
+      });
+
+      const buttonDelete = screen.getByLabelText(/Borrar/i);
+      await userEvent.click(buttonDelete);
+
+      await waitFor(() => {
+        expect(
+          screen.getByText(/No se encontro ninguna cita/i)
+        ).toBeInTheDocument();
+      });
+      await waitFor(() => {
+        expect(screen.getByDisplayValue("")).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe("Cuando oprimo cita aleatoria", () => {
+    it("Deberia traer data en la posición 1", async () => {
+      render(<Cita />);
+
+      await waitFor(() => {
+        expect(
+          screen.getByText(/No se encontro ninguna cita/i)
+        ).toBeInTheDocument();
+      });
+      const buttonDelete = screen.getByLabelText(/Borrar/i);
+      await userEvent.click(buttonDelete);
+
+      const buttonSearch = await screen.findByLabelText(
+        /Obtener Cita aleatoria/i
+      );
+      await userEvent.click(buttonSearch);
+
+      await waitFor(() => {
+        expect(
+          screen.getByText(
+            /I believe the children are the future... Unless we stop them nowThese are my only friends...grown-up nerds like Gore Vidal. And even he's kissed more boys than I ever will./i
+          )
         ).toBeInTheDocument();
       });
     });
