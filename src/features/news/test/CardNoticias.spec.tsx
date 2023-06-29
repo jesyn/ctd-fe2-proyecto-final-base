665a6c0c8127848b53f53b093aacfaf22d2946ef
Jesica Muñoz <jesydynathos@gmail.com>
2023-06-29 02:47:17 -0300

    fix test

--- src/features/news/test/CardNoticias.spec.tsx
+++ src/features/news/test/CardNoticias.spec.tsx
@@ -1,17 +1,20 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import CardNoticias, { ICardNoticias } from "../CardNoticias";
+import CardNoticias from "../CardNoticias";
 import { INoticiasNormalizadas } from "../noticias.contracts";
 
 describe("CardNoticias", () => {
   it("se hace clic en el botón 'Ver más'", async () => {
-    /*  const mockNoticias = [
+    const mockNoticias: INoticiasNormalizadas[] = [
       {
         id: 1,
         imagen: "imagen1.jpg",
         titulo: "Noticia 1",
         fecha: "2023-06-26",
         descripcionCorta: "Descripción corta de la noticia 1",
+        esPremium: true,
+        descripcion:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, sit nostrum culpa sunt eum quidem illo animi, numquam natus tenetur libero sint iusto. Harum molestias minima repellat in porro tenetur!",
       },
       {
         id: 2,
@@ -19,6 +22,9 @@ describe("CardNoticias", () => {
         titulo: "Noticia 2",
         fecha: "2023-06-27",
         descripcionCorta: "Descripción corta de la noticia 2",
+        esPremium: true,
+        descripcion:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quae perferendis ea fugiat voluptates neque vel, accusamus quibusdam dicta voluptate at dolores minus qui quidem impedit numquam a perspiciatis rerum.",
       },
     ];
     const handleClickMock = jest.fn();
@@ -33,6 +39,6 @@ describe("CardNoticias", () => {
     await waitFor(() => {
       expect(handleClickMock).toHaveBeenCalledTimes(1);
     });
-    //expect(handleClickMock).toHaveBeenCalledWith(mockNoticias[0]); */
+    expect(handleClickMock).toHaveBeenCalledWith(mockNoticias[0]);
   });
 });
--- src/features/news/test/Noticias.spec.tsx
+++ src/features/news/test/Noticias.spec.tsx
@@ -1,28 +1,108 @@
 import { render, screen } from "@testing-library/react";
 import Noticias from "../Noticias";
+import * as fakeRest from "./../fakeRest";
+import userEvent from "@testing-library/user-event";
 
 describe("Noticias", () => {
-  /*  beforeEach(() => {
-    jest.mock("./fakeRest", () => ({
-      obtenerNoticias: jest.fn(() => Promise.resolve([])),
-    }));
-  }); */
-
-  it("muestra el título 'Noticias de los Simpsons'", () => {
+  it("muestra el título 'Noticias de los Simpsons'", async () => {
     render(<Noticias />);
-    //const titulo = screen.getByText("Noticias de los Simpsons");
-    //expect(titulo).toBeInTheDocument();
+    expect(
+      await screen.findByText("Noticias de los Simpsons")
+    ).toBeInTheDocument();
   });
 
-  /*  it("muestra el componente CardNoticias", () => {
-    render(<Noticias />);
-    const cardNoticias = screen.getByTestId("card-noticias");
-    expect(cardNoticias).toBeInTheDocument();
-  }); */
-
-  /* it("no muestra el modal inicialmente", () => {
+  it("no muestra el modal inicialmente", () => {
     render(<Noticias />);
     const modal = screen.queryByTestId("modal");
     expect(modal).toBeNull();
-  }); */
+  });
+
+  it("esperar para obtener informacion", async () => {
+    jest.spyOn(fakeRest, "obtenerNoticias");
+    (fakeRest.obtenerNoticias as jest.Mock).mockImplementation(() => {
+      return [
+        {
+          id: 1,
+          titulo: "Noticia 1",
+          descripcion: "Descripción de la noticia 1",
+          fecha: new Date("2023-06-28"),
+          esPremium: false,
+          imagen: "imagen1.jpg",
+        },
+        {
+          id: 2,
+          titulo: "Noticia 2",
+          descripcion: "Descripción de la noticia 2",
+          fecha: new Date("2023-06-27"),
+          esPremium: true,
+          imagen: "imagen2.jpg",
+        },
+      ];
+    });
+    render(<Noticias />);
+    expect(await screen.findByText("Noticia 1")).toBeInTheDocument();
+    expect(await screen.findByText("Noticia 2")).toBeInTheDocument();
+  });
+
+  it("mostrar modal premium", async () => {
+    jest.spyOn(fakeRest, "obtenerNoticias");
+    (fakeRest.obtenerNoticias as jest.Mock).mockImplementation(() => {
+      return [
+        {
+          id: 1,
+          titulo: "Noticia 1",
+          descripcion: "Descripción de la noticia 1",
+          fecha: new Date("2023-06-28"),
+          esPremium: false,
+          imagen: "imagen1.jpg",
+        },
+        {
+          id: 2,
+          titulo: "Noticia 2",
+          descripcion: "Descripción de la noticia 2",
+          fecha: new Date("2023-06-27"),
+          esPremium: true,
+          imagen: "imagen2.jpg",
+        },
+      ];
+    });
+    render(<Noticias />);
+    const buttons = await screen.findAllByRole("button", { name: /Ver más/i });
+    const firstButton = buttons[0];
+
+    await userEvent.click(firstButton);
+
+    expect(await screen.findByTestId("modal")).toBeInTheDocument();
+  });
+
+  it("mostrar modal subscripcion", async () => {
+    jest.spyOn(fakeRest, "obtenerNoticias");
+    (fakeRest.obtenerNoticias as jest.Mock).mockImplementation(() => {
+      return [
+        {
+          id: 1,
+          titulo: "Noticia 1",
+          descripcion: "Descripción de la noticia 1",
+          fecha: new Date("2023-06-28"),
+          esPremium: false,
+          imagen: "imagen1.jpg",
+        },
+        {
+          id: 2,
+          titulo: "Noticia 2",
+          descripcion: "Descripción de la noticia 2",
+          fecha: new Date("2023-06-27"),
+          esPremium: true,
+          imagen: "imagen2.jpg",
+        },
+      ];
+    });
+    render(<Noticias />);
+    const buttons = await screen.findAllByRole("button", { name: /Ver más/i });
+    const secondButton = buttons[1];
+
+    await userEvent.click(secondButton);
+
+    expect(await screen.findByTestId("modal")).toBeInTheDocument();
+  });
 });
